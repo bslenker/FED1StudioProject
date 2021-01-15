b3557ac43cfadfac5e5b045a2ce398528704772a
bslenker <bslenker@gmail.com>
2021-01-14 22:09:17 -0600

    added Lightbox functionality to teams page with a js file

---
+++ FED1StudioProject/js/jquery.js
@@ -0,0 +1,23 @@
+﻿///<reference path="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"/>
+
+
+$(function () {
+    //....Custom Lightbox....//
+    //When a user clicks on a thumbnail img, display the lightbox container and insert <img>
+    $('.thumb').on('click', function () {
+        //Find the src attribute of the img tag that was clicked and store it as a var
+        var imgSrc = $(this).attr('src');
+
+        //Plug an img tag into the #lightbox-content
+        $('#lightbox-content').html('<img src="' + imgSrc + '" class="img-responsive"/>');
+
+        //Make the lightbox visible by fading in...
+        $('#lightbox-container').fadeIn(500);
+
+        //when the user clicks anywhere in the lightbox-container, fadeOut()
+        $('#lightbox-container').on('click', function () {
+            $(this).fadeOut(500);
+        });
+
+    });
+})
